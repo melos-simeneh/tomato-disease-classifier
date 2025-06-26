a7d204cbe958979ede502ecf6ec5a2f1bfd5b774
Melos <melos.simeneh@gmail.com>
2025-06-26 10:25:46 +0300

    add binary classifier to verify tomato leaf after CLIP leaf detection

--- backend/utils/tomato_leaf_detector.py
+++ backend/utils/tomato_leaf_detector.py
@@ -1,6 +1,8 @@
 from transformers import CLIPProcessor, CLIPModel, VisionEncoderDecoderModel,ViTImageProcessor,AutoTokenizer
 from PIL import Image
 import torch
+import tensorflow as tf
+import numpy as np
 
 clip_model_name = "openai/clip-vit-base-patch32"
 clip_model = CLIPModel.from_pretrained(clip_model_name)
@@ -11,6 +13,10 @@ caption_model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-i
 caption_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 caption_tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
+BINARY_MODEL_PATH = "model/binary_tomato_leaf_classifier.keras"
+BINARY_MODEL= tf.keras.models.load_model(BINARY_MODEL_PATH, compile=False)
+B_CLASS_NAMES = ['non_tomato_leaf', 'tomato_leaf']
+
 
 def generate_caption(image):
     if image.mode != "RGB":
@@ -19,6 +25,14 @@ def generate_caption(image):
     output_ids = caption_model.generate(pixel_values, max_length=16, num_beams=1)
     caption = caption_tokenizer.decode(output_ids[0], skip_special_tokens=True)
     return caption
+
+def binary_prediction(img):
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    predictions = BINARY_MODEL.predict(img_array, verbose=0)
+    predicted_class = B_CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = round(100 * np.max(predictions[0]), 2)
+    return predicted_class, confidence
 
 
 def is_tomato_leaf(input_image: Image.Image, confidence_threshold: float = 0.65) -> bool:
@@ -45,4 +59,11 @@ def is_tomato_leaf(input_image: Image.Image, confidence_threshold: float = 0.65)
     
     caption = generate_caption(input_image)
 
-    return tomato_related_prob >= confidence_threshold and any(phrase in caption for phrase in ["green plant", "green leaf"])
+    if (
+    tomato_related_prob >= confidence_threshold and
+    any(phrase in caption for phrase in ["green plant", "green leaf"])
+    ):
+        b_predicted_class, confidence = binary_prediction(input_image)
+        return b_predicted_class == "tomato_leaf"
+
+    return False
